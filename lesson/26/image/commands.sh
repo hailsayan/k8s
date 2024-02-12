2ad18e02dfe44aae12777715d981b548d92d1dfe
Psyon <sayan.h2020@gmail.com>
2024-02-12 22:57:25 +0330

    lesson 26

---
+++ lesson/26/image/commands.sh
@@ -0,0 +1,10 @@
+cd image/
+
+# Build the image
+docker build -f Dockerfile -t k8s-content-gen:v3 .
+
+# Load the image into the K8s cluster
+# Replace <YOUR_CLUSTER_NAME> with your kind cluster name (kind get clusters)
+kind load docker-image k8s-content-gen:v3 --name <YOUR_CLUSTER_NAME>
+
+cd ..
